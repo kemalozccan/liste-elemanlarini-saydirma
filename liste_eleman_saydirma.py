2b6993a7cfa8dfbc49cb8b9ee75bf0ae905915fd
Kemal Özcan <129630958+kemalozccan@users.noreply.github.com>
2023-05-12 22:24:20 +0300

    Add files via upload

---
+++ liste_eleman_saydirma.py
@@ -0,0 +1,8 @@
+liste = ["banana", "orange", "apple", "banana", "apple", 45, 23, 45, 89, "apple"]
+a = {}
+for i in liste:
+    a.update({i: liste.count(i)})
+#     a[i] = liste.count(i)
+
+for key1 in a.keys():
+    print(key1, " : ", a[key1], " Piece")
